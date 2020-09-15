3837aeece2b4a38e60ca7f6934968b9644ddee04
Kevin Grow <kevingrow11@gmail.com>
2020-09-15 22:15:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,3 +14,10 @@ def loop_message_n_times(message,number)
   end 
 end 
 
+def output_array(message,number)
+  counter = 0
+  while counter <= number do 
+    puts message
+    counter += 1 
+  end 
+end 
