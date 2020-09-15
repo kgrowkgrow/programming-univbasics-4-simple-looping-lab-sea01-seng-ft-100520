17fe5c84831e5e7443410250bb557c1fd323a75b
Kevin Grow <kevingrow11@gmail.com>
2020-09-15 22:25:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,12 +24,13 @@ end
 
 def return_string_array(array)
   counter = 0
-#  new_array = []
+new_array = []
   while counter <= array.length do 
     array[counter].to_s
+    new_array.push(array[counter])
     counter += 1 
   end 
-  array
+  new_array
 end 
 
 
